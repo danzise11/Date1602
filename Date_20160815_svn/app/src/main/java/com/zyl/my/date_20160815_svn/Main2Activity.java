fa0d1939d1ffb9a54e7c700cc1a07ee5743bfdf1
danzise <1120053015@qq.com>
2016-08-16 15:20:06 +0800

    zaiw

---
+++ Date_20160815_svn/app/src/main/java/com/zyl/my/date_20160815_svn/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.zyl.my.date_20160815_svn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+       TextView textView_main_a = (TextView) findViewById(R.id.textView_main_a);
+    }
+}
